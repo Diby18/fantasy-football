51412f1a4bad440f26115e974cb63b8d422eb5ec
Mattia Dibiliogit <dibiliomattia@gmail.com>
2022-07-25 23:37:58 +0200

    feat: implementazione

---
+++ class_person/persona.cpp
@@ -0,0 +1,15 @@
+#include "persona.h"
+
+Persona::Persona(const unsigned short id, const std::string nome, const unsigned short crediti)
+{
+    this->id = id;
+
+    this->nome = nome;
+
+    this->crediti = crediti;
+}
+
+std::ostream& Persona::getInfo(std::ostream &os) const
+{
+    return os<<"ID: "<<this->id<<"\nNome: "<<this->nome<<"\nCrediti: "<<this->crediti<<std::endl;
+}
