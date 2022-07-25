c1d4dd53ce9844b80a5f799b71a607884b5c0a96
Mattia Dibiliogit <dibiliomattia@gmail.com>
2022-07-26 00:56:08 +0200

    feat: implementazione main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include "class_allenatore/allenatore.h"
+
+using namespace std;
+
+int main()
+{
+    Allenatore a("mattia",1000);
+
+    cout<<a;
+
+    cout<<"\n\nFine";
+
+    return 0;
+}
