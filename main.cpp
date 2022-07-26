b2361867f16fdf7738d544a34c06c44e53b8da06
Mattia Dibiliogit <dibiliomattia@gmail.com>
2022-07-26 02:45:50 +0200

    chore: semplici test

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
-#include <fstream>
 #include "class_allenatore/allenatore.h"
+#include "class_giocatore/giocatore.h"
 
 using namespace std;
 
 int main()
 {
     Allenatore a("mattia",1000);
+
+    a.operazione(new Giocatore("Immobile",100,'A'));
+    a.operazione(new Giocatore("Lukaku",100,'A'));
 
     cout<<a;
 
