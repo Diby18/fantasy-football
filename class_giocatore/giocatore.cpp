1b72092ca39915a5e4652e374ccf9c6f621d4810
Mattia Dibiliogit <dibiliomattia@gmail.com>
2022-07-26 02:05:30 +0200

    feat: introduzione variabile ruolo

--- class_giocatore/giocatore.cpp
+++ class_giocatore/giocatore.cpp
@@ -2,13 +2,22 @@
 
 Giocatore::Giocatore() { }
 
-Giocatore::Giocatore(const std::string nome, const unsigned short prezzo): Persona(nome,prezzo) { }
+Giocatore::Giocatore(const std::string nome, const unsigned short prezzo, const char ruolo): Persona(nome,prezzo) 
+{ 
+    this->ruolo = ruolo;
+}
 
 std::ostream& Giocatore::getInfo(std::ostream &os) const
 {
+    os<<"Ruolo: "<<this->ruolo<<"\n";
+
     return Persona::getInfo(os);
 }
 
+Giocatore::operator std::string() const
+{
+    return '(' + std::string(1,this->ruolo) + ") " + this->nome + " " + std::to_string(this->crediti);
+}
 
 //overloading << per Giocatore
 std::ostream& operator<<(std::ostream &os, const Giocatore &giocatore)
--- class_giocatore/giocatore.h
+++ class_giocatore/giocatore.h
@@ -5,10 +5,14 @@
 
 class Giocatore: public Persona
 {
+    char ruolo;
+
     public:
         Giocatore();
-        Giocatore(const std::string, const unsigned short);
+        Giocatore(const std::string, const unsigned short,const char);
         std::ostream& getInfo(std::ostream&) const;
+
+        operator std::string() const;
 
         friend std::ostream& operator<<(std::ostream&,const Giocatore&);
 };
