1612e517f6266711c06b9959361b81dd2713ac76
Mattia Dibiliogit <dibiliomattia@gmail.com>
2022-07-27 19:54:41 +0200

    feat: implementato metodo virtual e modifica classe

--- class_allenatore/allenatore.cpp
+++ class_allenatore/allenatore.cpp
@@ -5,44 +5,25 @@
 
 Allenatore::Allenatore(const std::string nome, const unsigned short crediti): Persona(nome,crediti) { }
 
-void Allenatore::operazione(const Persona *giocatore)
+void Allenatore::eseguiOperazione(Persona *giocatore)
 {
     std::string path = "./output/" + this->nome + ".txt";
 
     std::ofstream squadra(path, std::ios::app);
 
-    if(typeid(*giocatore) == typeid(Giocatore))
-    {
-        Giocatore *nuovo = (Giocatore*) giocatore;
+    this->listaGiocatori.push_front(giocatore);
 
-        this->listaGiocatori.push_front(new Giocatore(*nuovo));
+    //squadra<<(std::string) *giocatore<<std::endl; 
 
-        squadra<<(std::string) *nuovo<<std::endl; 
-    }
-
-    else { std::cerr<<"Inserimento non valido\n"; return; }
+   // else { std::cerr<<"Inserimento non valido\n"; return; }
     
     this->crediti -= giocatore->getCrediti(); 
 }
 
-void Allenatore::getInfo(std::ostream &os) const
-{
-    system("cls");
-
-    os<<"Allenatore\n\n";
-
-    Persona::getInfo(os);
-
-    os<<"Giocatori acquistati\n\n";
-
-    for(auto giocatore : this->listaGiocatori) giocatore->getInfo(os);
-}
-
-
 //overloading << per Allenatore
 std::ostream& operator<<(std::ostream &os, const Allenatore &allenatore)
 {
-    allenatore.getInfo(os);
+    //allenatore.getInfo(os);
 
     return os;
 }
