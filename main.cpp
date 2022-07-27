def7886c35b27f18a4ba98ba1c4cd26259d933c8
Mattia Dibiliogit <dibiliomattia@gmail.com>
2022-07-27 20:49:41 +0200

    feat: implementazione input dati e overloading<<
    Input numero di crediti dell'asta, fantallenatori

--- main.cpp
+++ main.cpp
@@ -1,17 +1,97 @@
 #include <iostream>
+#include <limits>
+#include <vector>
+#include <typeinfo>
+#include <cctype>
+
 #include "class_allenatore/allenatore.h"
 #include "class_giocatore/giocatore.h"
 
 using namespace std;
 
+bool checkInput(istream &is)
+{
+    if(cin.fail())
+    {
+        cerr<<"\nERRORE!!\nE' possibile inserire solo numeri\n";
+
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        return false;
+    }
+
+    return true;
+}
+
+void input(vector<Persona*> &allenatori, const unsigned short creditiAsta)
+{
+    unsigned short numeroAllenatori;
+    string nomeAllenatore;
+
+    do
+    {
+        cout<<"Inserire il numero di partecipanti: ";
+        cin>>numeroAllenatori;
+
+    } while(!checkInput(cin));
+
+    for(unsigned i = 0; i<numeroAllenatori; i++)
+    {
+        system("cls");
+
+        cout<<"Inserisci l'allenatore "<<i+1<<": ";
+        cin>>nomeAllenatore;
+
+        nomeAllenatore[0] = toupper(nomeAllenatore[0]);
+
+        allenatori.push_back(new Allenatore(nomeAllenatore, creditiAsta));
+    }
+}
+
+void input(vector<Persona*> &allenatori)
+{
+    bool errore;
+    unsigned short crediti;
+
+    system("cls");
+
+    do
+    {
+        cout<<"Inserire i crediti dell'asta: ";
+        cin>>crediti;
+
+    } while(!checkInput(cin));
+
+    system("cls");
+
+    input(allenatori,crediti);
+
+    system("cls");
+}
+
+ostream& operator<<(ostream &os, const vector<Persona*> vec)
+{
+    for(auto i : vec)
+    {
+        if(typeid(*i) == typeid(Allenatore))
+        {
+            os<< *( (Giocatore*) i );
+        }
+
+    }
+
+    return os;
+}
+
 int main()
 {
-    Allenatore a("mattia",1000);
+    vector<Persona*> allenatori;
+    vector<Persona*> giocatoriAcquistati;
 
-    a.operazione(new Giocatore("Immobile",100,'A'));
-    a.operazione(new Giocatore("Lukaku",100,'A'));
+    input(allenatori);
 
-    cout<<a;
+    cout<<allenatori;
 
     cout<<"\n\nFine";
 
