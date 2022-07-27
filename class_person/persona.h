8b28ba2de1a836ce347a046bc4848d8d26c253bd
Mattia Dibiliogit <dibiliomattia@gmail.com>
2022-07-27 19:51:56 +0200

    feat: aggiunta funzione virtuale pura

--- class_person/persona.h
+++ class_person/persona.h
@@ -13,6 +13,7 @@ class Persona
         Persona();
         Persona(const std::string, const unsigned short);
         virtual void getInfo(std::ostream&) const;
+        virtual void eseguiOperazione(Persona*) = 0;
 
         std::string getNome() const;
         unsigned short getCrediti() const;
