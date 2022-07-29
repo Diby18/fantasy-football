0302b3c1925b9e298c4ae22001963f197896e9d8
Mattia Dibiliogit <dibiliomattia@gmail.com>
2022-07-30 00:56:19 +0200

    feat: implementazione metodi e modifica variabile ruolo

--- class_giocatore/giocatore.cpp
+++ class_giocatore/giocatore.cpp
@@ -13,6 +13,10 @@ void Giocatore::eseguiOperazione(Persona *proprietario)
     this->proprietario = proprietario;
 }
 
+char Giocatore::getRuolo() const { return this->ruolo; }
+
+const Persona* const Giocatore::getProprietario() const { return this->proprietario; }
+
 Giocatore::operator std::string() const
 {
     return '(' + std::string(1,this->ruolo) + ") " + this->nome + " " 
@@ -20,18 +24,20 @@ Giocatore::operator std::string() const
 }
 
 //overloading << per Giocatore
-/*std::ostream& operator<<(std::ostream &os, const Giocatore &giocatore)
+std::ostream& operator<<(std::ostream &os, const Giocatore &giocatore)
 {
     os<<"Ruolo: "<<giocatore.ruolo<<std::endl;
 
     giocatore.getInfo(os);
 
     return os;
-}*/
+}
 
 std::istream& operator>>(std::istream &is, Giocatore &giocatore)
 {
     is>>giocatore.ruolo>>giocatore.nome>>giocatore.crediti;
     
+    giocatore.ruolo = toupper(giocatore.ruolo);
+
     return is;
 }
