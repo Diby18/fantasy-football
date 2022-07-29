5c822ee3215cd6496dad23873829358ae6e9f87e
Mattia Dibiliogit <dibiliomattia@gmail.com>
2022-07-29 22:21:16 +0200

    scrittura su file non più esegui dal metodo

--- class_allenatore/allenatore.cpp
+++ class_allenatore/allenatore.cpp
@@ -1,5 +1,4 @@
 #include "allenatore.h"
-#include "../class_giocatore/giocatore.h"
 #include <fstream>
 #include <typeinfo>
 
@@ -7,15 +6,7 @@ Allenatore::Allenatore(const std::string nome, const unsigned short crediti): Pe
 
 void Allenatore::eseguiOperazione(Persona *giocatore)
 {
-    std::string path = "./output/" + this->nome + ".txt";
-
-    std::ofstream squadra(path, std::ios::app);
-
     this->listaGiocatori.push_front(giocatore);
-
-    //squadra<<(std::string) *giocatore<<std::endl; 
-
-   // else { std::cerr<<"Inserimento non valido\n"; return; }
     
     this->crediti -= giocatore->getCrediti(); 
 }
