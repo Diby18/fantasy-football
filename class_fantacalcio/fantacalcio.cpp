d42043fb65cae9db880489d002618aba780d435d
Mattia Dibiliogit <dibiliomattia@gmail.com>
2022-07-29 22:20:13 +0200

    implementazione

--- class_fantacalcio/fantacalcio.cpp
+++ class_fantacalcio/fantacalcio.cpp
@@ -1,10 +1,36 @@
 #include "fantacalcio.h"
 
-Fantacalcio::Fantacalcio(const unsigned short crediti): creditiAsta(crediti) { }
+Fantacalcio::Fantacalcio() { }
+
+Persona* Fantacalcio::cercaAllenatore(const std::string nome) const
+{
+    for(auto i : this->allenatori)
+
+        if(i->getNome() == nome) return i;
+
+    return nullptr;
+}
 
 void Fantacalcio::aggiungiAllenatore(Persona *allenatore)
 {
     this->allenatori.push_back(allenatore);
+}
+
+void Fantacalcio::acquistaGiocatore(std::string nomeAllenatore, Persona *giocatore)
+{
+    Persona *allenatore = this->cercaAllenatore(nomeAllenatore);
+
+    if(!allenatore) { std::cerr<<"\nAllenatore non trovato\n"; return; }
+
+    allenatore->eseguiOperazione(giocatore);
+
+    giocatore->eseguiOperazione(allenatore);
+
+    std::string path = "./output/" + allenatore->getNome() + ".txt";
+
+    std::ofstream squadra(path, std::ios::app);
+
+    squadra<< (std::string) *( (Giocatore*) giocatore );
 }
 
 void Fantacalcio::stampaAllenatori() const
