7a4980ae269675421c21c42806bd9ee8bde9134d
Mattia Dibiliogit <dibiliomattia@gmail.com>
2022-07-30 00:54:51 +0200

    feat: getRuolo, getProprietario

--- class_giocatore/giocatore.h
+++ class_giocatore/giocatore.h
@@ -1,6 +1,7 @@
 #ifndef GIOCATORE_H
 #define GIOCATORE_H
 
+#include <ctype.h>
 #include "../class_person/persona.h"
 
 class Giocatore: public Persona
@@ -11,12 +12,15 @@ class Giocatore: public Persona
     public:
         Giocatore();
         Giocatore(const std::string, const unsigned short,const char);
+        
+        char getRuolo() const;
+        const Persona * const getProprietario() const;
 
         void eseguiOperazione(Persona*);
 
         operator std::string() const;
 
-        //friend std::ostream& operator<<(std::ostream&,const Giocatore&);
+        friend std::ostream& operator<<(std::ostream&,const Giocatore&);
         friend std::istream& operator>>(std::istream&, Giocatore&);
 };
 
